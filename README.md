ca376eb8a9ef35a01c074c6a878c4c5758f74f72
jandoant <jandoant@outlook.de>
2016-12-13 18:29:24 +0100

    v.03 - UI Update

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # justjava
 
-### v1.0 UI Update
+### v0.1 UI Update
 * Linear Layout
 * TextViews zur Anzeige von Menge und Preis
 * Buttons zur Steuerung der Menge
 * Button zur Berechnung der Bestellung
 
 ---------------------------------------------------
+### v0.2 Java Update
+* Add und Remove Coffee - Buttons inkrementieren oder dekrementieren die Menge
+* aktuelle Menge wird angezeigt
+* Preis wird berechnet und angezeigt nach Klick auf Order Button
+
+---------------------------------------------------
+### v0.3 UI Update
+* Quantity-Picker: Buttons nicht mehr vertikal sondern horizontal angeordnet
+---------------------------------------------------
+### v0.4 Java + UI Update
+* Added Whipped Cream, Chocolate Checkbox +  its functionality
+* Erstellung und Anzeige von Order Summary, wenn Bestellung abgegeben wird
+* Scroll View added
+* Text Input Field for username added
+* Extra Charge for Chocolate and Cream implemented
--- app/src/main/java/com/gmail/jandoant/justjava/MainActivity.java
+++ app/src/main/java/com/gmail/jandoant/justjava/MainActivity.java
@@ -3,12 +3,18 @@ package com.gmail.jandoant.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int numberOfCoffees, pricePerCoffee, price;
-    TextView txt_quantity, txt_price;
+    private int numberOfCoffees, pricePerCoffee, price;
+    private EditText editText_clientName;
+    private TextView txt_quantity, txt_order;
+    private CheckBox chkBox_cream, chkBox_chocolate;
+    private String clientName;
+    private boolean hasWhippedCream, hasChocolate;
 
 
     @Override
@@ -19,15 +25,27 @@ public class MainActivity extends AppCompatActivity {
         //initial Values
         numberOfCoffees = 2;
         pricePerCoffee = 5;
+        clientName = null;
 
         //initUI
+        editText_clientName = (EditText) findViewById(R.id.editText_clientName);
         txt_quantity = (TextView) findViewById(R.id.txt_quantity);
-        txt_price = (TextView) findViewById(R.id.txt_price);
-        updateOrder();
+        txt_order = (TextView) findViewById(R.id.txt_order_summary);
+        chkBox_cream = (CheckBox) findViewById(R.id.chkBox_cream);
+        chkBox_chocolate = (CheckBox) findViewById(R.id.chkBox_chocolate);
+        displayQuantity();
     }
 
     public void submitOrder(View view) {
-        updateOrder();
+        assembleOrder();
+        displayOrderSummary();
+    }
+
+    private void assembleOrder() {
+        hasWhippedCream = chkBox_cream.isChecked();
+        hasChocolate = chkBox_chocolate.isChecked();
+        clientName = String.valueOf(editText_clientName.getText());
+        calulatePrice();
     }
 
     public void addCoffee(View view) {
@@ -47,22 +65,31 @@ public class MainActivity extends AppCompatActivity {
         displayQuantity();
     }
 
-    private void updatePrice() {
+    private void calulatePrice() {
+        //Pure Coffee Price
         price = numberOfCoffees * pricePerCoffee;
+        //Extra Charge
+        if (hasWhippedCream) {
+            price += 1;
+        }
+        if (hasChocolate) {
+            price += 2;
+        }
     }
 
-    private void displayPrice() {
-        txt_price.setText("$" + price);
+    private void displayOrderSummary() {
+
+        txt_order.setText(
+                "Name: " + clientName + "\n" +
+                        "Add Whipped Cream? " + hasWhippedCream + "\n" +
+                        "Add Chocolate? " + hasChocolate + "\n" +
+                        "Quantity: " + numberOfCoffees + "\n" +
+                        "Total: " + price + "$\n" +
+                        "Thank you!");
     }
 
     private void displayQuantity() {
         txt_quantity.setText("" + numberOfCoffees);
-    }
-
-    private void updateOrder() {
-        displayQuantity();
-        updatePrice();
-        displayPrice();
     }
 
 
