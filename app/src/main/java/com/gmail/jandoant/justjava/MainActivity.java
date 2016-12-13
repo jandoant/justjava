acb7afc74c3c1d847a6671f834115ff26e0afe9a
jandoant <jandoant@outlook.de>
2016-12-13 11:40:19 +0100

    v.02 - Java-Funktionalität hinzugefügt

--- app/src/main/java/com/gmail/jandoant/justjava/MainActivity.java
+++ app/src/main/java/com/gmail/jandoant/justjava/MainActivity.java
@@ -2,6 +2,7 @@ package com.gmail.jandoant.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initial Values
+        numberOfCoffees = 2;
+        pricePerCoffee = 5;
+
+        //initUI
+        txt_quantity = (TextView) findViewById(R.id.txt_quantity);
+        txt_price = (TextView) findViewById(R.id.txt_price);
+        updateOrder();
+    }
+
+    public void submitOrder(View view) {
+        updateOrder();
+    }
+
+    public void addCoffee(View view) {
+        numberOfCoffees++;
+        displayQuantity();
+    }
+
+    public void removeCoffee(View view) {
+
+        //keine negativen Kaffeemengen
+        if (numberOfCoffees <= 1) {
+            numberOfCoffees = 0;
+        } else {
+            numberOfCoffees--;
+        }
+
+        displayQuantity();
+    }
+
+    private void updatePrice() {
+        price = numberOfCoffees * pricePerCoffee;
+    }
+
+    private void displayPrice() {
+        txt_price.setText("$" + price);
+    }
+
+    private void displayQuantity() {
+        txt_quantity.setText("" + numberOfCoffees);
+    }
+
+    private void updateOrder() {
+        displayQuantity();
+        updatePrice();
+        displayPrice();
     }
 
 
