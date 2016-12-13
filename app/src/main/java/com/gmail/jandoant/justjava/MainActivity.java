3f5f5fac7f61f4bcf1f8e0ef399ec3d0375f90d9
jandoant <jandoant@outlook.de>
2016-12-13 11:21:48 +0100

    v0.1 - UI Update

--- app/src/main/java/com/gmail/jandoant/justjava/MainActivity.java
+++ app/src/main/java/com/gmail/jandoant/justjava/MainActivity.java
@@ -2,12 +2,20 @@ package com.gmail.jandoant.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int numberOfCoffees, pricePerCoffee, price;
+    TextView txt_quantity, txt_price;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
